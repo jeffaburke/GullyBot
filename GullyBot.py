4e654115426b3fa59bd276bf4b7e0ef03c33eaa7
leftguard63 <41095357+leftguard63@users.noreply.github.com>
2019-01-24 11:00:55 -0500

    Update GullyBot.py

--- GullyBot.py
+++ GullyBot.py
@@ -15,5 +15,9 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     await member.add_roles("537626518566862849")
+    
+@bot.command
+async def test():
+    await bot.say('The bot is being programmed and this is a test :)')
 
 bot.run("NTM3NjI4MDIzNzUxNDQyNDUz.DypeTA.lYbx_vwiFjtQZT754IxkE8WD1BU")
