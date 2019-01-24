e54e32f92a5e74f548b797a58e951aead61533d3
leftguard63 <41095357+leftguard63@users.noreply.github.com>
2019-01-23 22:09:21 -0500

    Add files via upload

---
+++ GullyBot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.AutoShardedBot(command_prefix='!', activity=discord.Streaming(name='Being programmed :) Gully Fan Bot', url='http://www.twitch.tv/imgully'))
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print("---Bot---")
+    print(bot.user.id)
+    print("---is ready!---")
+
+@bot.event
+async def on_member_join(member):
+    await member.add_roles("537626518566862849")
+
+bot.run("NTM3NjI4MDIzNzUxNDQyNDUz.DypeTA.lYbx_vwiFjtQZT754IxkE8WD1BU")
