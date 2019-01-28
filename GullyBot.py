b7d65c6b96375bb492246a5d05399b0ffe08c095
leftguard63 <41095357+leftguard63@users.noreply.github.com>
2019-01-28 15:02:47 -0500

    Update GullyBot.py

--- GullyBot.py
+++ GullyBot.py
@@ -19,9 +19,20 @@ async def on_member_join(member):
 @bot.command(pass_context=True)
 async def test(ctx):
     await ctx.channel.send('The bot is being programmed and this is a test :)')
+
+@bot.command(pass_context=True)
+async def clear(ctx, number=100):
+    mgs = [] 
+    number = int(number) + 1
+    async for x in ctx.channel.history(limit = number):
+        mgs.append(x)
+    await ctx.channel.delete_messages(mgs)
+    await ctx.channel.send(f'`{number} messages have been deleted!`')
+
     
 @bot.command(pass_context=True)
 async def gully(ctx):
     await ctx.channel.send(f'{ctx.author.mention}, Gully\'s twitch is: \nhttps://twitch.tv/imgully\nHis twitter is:\nhttps://twitter.com/ImGullyTV\nHis YouTube channel is coming soon!')
 
 bot.run("NTM3NjI4MDIzNzUxNDQyNDUz.DypeTA.lYbx_vwiFjtQZT754IxkE8WD1BU")
+
