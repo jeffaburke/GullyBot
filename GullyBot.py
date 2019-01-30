dede84510a7507a17b6a1425e3fdb82011e7f043
leftguard63 <41095357+leftguard63@users.noreply.github.com>
2019-01-29 20:13:35 -0500

    Update GullyBot.py

--- GullyBot.py
+++ GullyBot.py
@@ -13,7 +13,7 @@ async def on_ready():
     print("---is ready!---")
 
 @bot.event
-async def on_member_join(member):
+async def on_member_join(ctx, member):
     await member.add_roles("537626518566862849")
     
 @bot.command(pass_context=True)
@@ -21,18 +21,42 @@ async def test(ctx):
     await ctx.channel.send('The bot is being programmed and this is a test :)')
 
 @bot.command(pass_context=True)
-async def clear(ctx, number=100):
+@commands.has_any_role('Gully', "Moderators")
+async def clear(ctx, number=100, silent='a'):
     mgs = [] 
     number = int(number) + 1
     async for x in ctx.channel.history(limit = number):
         mgs.append(x)
     await ctx.channel.delete_messages(mgs)
-    await ctx.channel.send(f'`{number} messages have been deleted!`')
+    silent = str(silent)
+    if silent == 'a':
+        await ctx.channel.send(f'`{number} messages have been deleted!`')
+
+@bot.command(pass_context=True)
+@commands.has_any_role('Gully', "Moderators")
+async def announce(ctx, say=''):
+    await ctx.send(say)
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send('`You must be a moderator to do this command!`')
+
 
     
 @bot.command(pass_context=True)
 async def gully(ctx):
     await ctx.channel.send(f'{ctx.author.mention}, Gully\'s twitch is: \nhttps://twitch.tv/imgully\nHis twitter is:\nhttps://twitter.com/ImGullyTV\nHis YouTube channel is coming soon!')
 
-bot.run("NTM3NjI4MDIzNzUxNDQyNDUz.DypeTA.lYbx_vwiFjtQZT754IxkE8WD1BU")
+@bot.command(pass_context=True)
+async def help(ctx):
+    author = ctx.author
+    help = discord.Embed(color=discord.Color.gold())
+    help.set_author(name='Heres all the commands')
+    help.add_field(name='Commands', value='!clear - This is a moderator only command that clears a certain amount of messages!\n!gully - Show\'s all of gully\'s social\'s\nThat\'s it so far!')
 
+    try:
+        await ctx.author.send(embed=help)
+    except discord.Forbidden:
+        await ctx.send(f'{ctx.author.mention}, I couldn\'t send a message to you.')
+
+bot.run("NTM3NjI4MDIzNzUxNDQyNDUz.DypeTA.lYbx_vwiFjtQZT754IxkE8WD1BU")
